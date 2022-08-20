d2f9d6daf1122db90974b6929828944629ebdaac
skyhihi <fahja246@gmail.com>
2022-08-20 20:19:14 +0700

    edit home

--- client/src/components/pages/home/Home.js
+++ client/src/components/pages/home/Home.js
@@ -3,30 +3,33 @@ import React from "react";
 import { Button } from "antd";
 import "./home.css";
 import Image1 from "../../assets/home-zoom.svg";
-//import Bg1 from "../../assets/home-layered-waves2.svg";
 
 const Home = () => {
   return (
     <>
-      <div className="home__container">
-        {/** <img src={Bg1} alt="" />*/}
-        <div className="home__content">
-          <img src={Image1} alt="" />
-          <p className="home__text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Voluptatibus mollitia ea, eaque enim illum facere. Tempora, illum
-            nesciunt labore repellendus, assumenda sunt maiores veritatis sed
-            error maxime dolores, minima optio.
-          </p>
-          <div className="home__btn">
-            <Button
-              type="primary"
-              shape="round"
-              size="large"
-              //icon={<DownloadOutlined />}
-            >
-              เริ่มทำแบบสอบถาม
-            </Button>
+      <div className="home__bg">
+        <div className="home__container">
+          <div className="home__content">
+            <img src={Image1} alt="" />
+            <div className="home__text">
+              <h3>ยินดีต้อนรับ</h3>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+                placeat fugiat adipisci ad! Magnam vel labore rem rerum ex
+                perspiciatis doloribus eaque, omnis nostrum! Culpa excepturi
+                dolores recusandae earum reprehenderit.
+              </p>
+            </div>
+            <div className="home__btn">
+              <Button
+                type="primary"
+                shape="round"
+                size="large"
+                //icon={<DownloadOutlined />}
+              >
+                เริ่มทำแบบสอบถาม
+              </Button>
+            </div>
           </div>
         </div>
       </div>
