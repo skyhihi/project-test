d7a635a179106d969248647470b59776e97a6a9b
skyhihi <fahja246@gmail.com>
2022-08-24 21:04:44 +0700

    fah update 23-8

--- client/src/components/pages/admin/MenuAdmin.js
+++ client/src/components/pages/admin/MenuAdmin.js
@@ -22,40 +22,29 @@ function getItem(label, key, icon, children, type, link) {
 
 const items = [
   getItem(
-    "HomeAdmin",
+    "General Information",
     "ha",
     <Link to="/admin">
       <NumberOutlined />
     </Link>
   ),
   getItem(
-    "Question",
+    "Student Information",
     "q",
     <Link to="/admin/question">
       <QuestionCircleOutlined />
     </Link>
   ),
-  getItem(
-    "Detail",
-    "dt",
-    <Link to="/admin/detail-types">
-      <AppstoreOutlined />
-    </Link>
-  ),
 
-  getItem("Navigation One", "sub1", <MailOutlined />, [
-    getItem("Option 1", "1"),
-    getItem("Option 2", "2"),
-    getItem("Option 3", "3"),
-    getItem("Option 4", "4"),
+  getItem("Question", "sub1", <MailOutlined />, [
+    getItem("ADD", "1"),
+    getItem("EDIT", "2"),
+    getItem("DELETE", "3"),
   ]),
-  getItem("Navigation Two", "sub2", <AppstoreOutlined />, [
-    getItem("Option 5", "5"),
-    getItem("Option 6", "6"),
-    getItem("Submenu", "sub3", null, [
-      getItem("Option 7", "7"),
-      getItem("Option 8", "8"),
-    ]),
+  getItem("Detail", "sub2", <AppstoreOutlined />, [
+    getItem("ADD", "1"),
+    getItem("EDIT", "2"),
+    getItem("DELETE", "3"),
   ]),
   getItem("Navigation Three", "sub4", <SettingOutlined />, [
     getItem("Option 9", "9"),
