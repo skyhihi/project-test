5204c72d2e3dd913e17b96ed87d539fb76bd112a
skyhihi <fahja246@gmail.com>
2022-08-28 01:03:39 +0700

    remove .env file from git

--- server/config/conDB.js
+++ server/config/conDB.js
@@ -1,10 +1,11 @@
 const mariadb = require("mariadb");
 
 const pool = mariadb.createPool({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "demodb",
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASS,
+  database: process.env.DATABASE,
+  port: process.env.PORTMR,
 });
 
 const connectDB = async () => {
