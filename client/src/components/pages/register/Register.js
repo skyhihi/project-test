614990ff770c232cb8a4c0ab847f8722dcc37da8
timmi <panisaracha.25@gmail.com>
2022-08-31 17:09:09 +0700

    TT update 31/08 1

--- client/src/components/pages/register/Register.js
+++ client/src/components/pages/register/Register.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+//import { useDispatch } from 'react-redux'
+//import { useNavigate } from "react-router-dom";
 //import type { SizeType } from "antd/es/config-provider/SizeContext";
 import {
   UserOutlined,
@@ -13,7 +16,45 @@ import {
 import { Input, Tooltip, Space, Button } from "antd";
 import "./register.css";
 
-const Register = () => (
+
+const Register = () => {
+  
+const [value, setValue] = useState({
+  name:"",
+  username: "",
+  password: "",
+  confirm_password: "",
+  
+});
+const onClick = (e) => {
+  //e.preventDefault(); //
+  console.log(value);
+  if (value.password !== value.confirm_password) {
+    toast.error("Password not match");
+  } else {
+    Register(value)
+      .then((res) => {
+        console.log(res.data);
+        toast.success(res.data);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+        toast.error(err.response.data);
+      });
+  }
+};
+
+const onChange = (e) => {
+  console.log(e.target.name);
+  console.log(e.target.value);
+  setValue({
+    ...value,
+    [e.target.name]: e.target.value,
+    [e.target.username]: e.target.value,
+  });
+  
+};
+return(
   <>
     <div className="register__bg"></div>
     <div className="register__content">
@@ -25,8 +66,11 @@ const Register = () => (
           </h1>
           <br />
           <Input
+            type="text"
+            name="name"
             size="large"
-            placeholder=" Please Enter Your Name"
+            placeholder=" Please Enter Your Name" 
+            onChange={onChange}
             prefix={<UserOutlined className="site-form-item-icon" />}
             suffix={
               <Tooltip title="Extra Information">
@@ -37,8 +81,11 @@ const Register = () => (
           <br />
           <br />
           <Input
+            type="text"
+            name="username"
             size="large"
             placeholder=" Please Enter Your Username"
+            onChange={onChange}
             prefix={<UserAddOutlined />}
             suffix={
               <Tooltip title="Extra Information">
@@ -50,8 +97,10 @@ const Register = () => (
           <br />
           <Space direction="vertical" style={{ width: "100%" }}>
             <Input.Password
+              name="password"
               size="large"
               placeholder=" Please Input Your Password"
+              onChange={onChange}
               prefix={<LockFilled />}
               iconRender={(visible) =>
                 visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
@@ -62,8 +111,10 @@ const Register = () => (
           <br />
           <Space direction="vertical" style={{ width: "100%" }}>
             <Input.Password
+              name="confirm_password"
               size="large"
               placeholder=" Please Confirm Your Password"
+              onChange={onChange}
               prefix={<LockFilled />}
               iconRender={(visible) =>
                 visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
@@ -73,8 +124,9 @@ const Register = () => (
             <br />
             <Link to="/login">
             <Button
-              type="secondary"
+              type="submit"
               shape="round"
+              onClick={onClick}
               //size="large"
               style={{ float: "right" }}
               icon={<LoginOutlined />}
@@ -89,5 +141,7 @@ const Register = () => (
     </div>
   </>
 );
+};
+           
 
 export default Register;
