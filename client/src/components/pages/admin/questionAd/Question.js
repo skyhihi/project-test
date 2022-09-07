30f5b465324889027342e90a76cf87871dde8f7b
skyhihi <fahja246@gmail.com>
2022-09-07 22:18:43 +0700

    fah update 7-9 2

--- client/src/components/pages/admin/questionAd/Question.js
+++ client/src/components/pages/admin/questionAd/Question.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import MenuAdmin from "../MenuAdmin";
-import { Select, Input, Tooltip, Button } from "antd";
+import { Select, Input, Tooltip, Button, Modal } from "antd";
 import {
   listQuestions,
   removeQuestion,
   createQuestion,
+  changeQuestion,
+  changeType,
 } from "../../../functions/question";
 
 import { type } from "../../../functions/type";
@@ -21,7 +23,7 @@ const Question = () => {
 
   const handleChange = (value) => {
     setSelected(value);
-    console.log(`selected ${value}`);
+    //console.log(`selected ${value}`);
   };
 
   const [questions, setQuestions] = useState([]);
@@ -60,53 +62,124 @@ const Question = () => {
   }, []);
 
   //===========================
-  const onClick = (e) => {
-    e.preventDefault(); //
-    console.log(value);
-
-    createQuestion(value)
-      .then((res) => {
-        console.log(res.data);
-        toast.success(res.data.status);
-        loadData();
-      })
-      .catch((err) => {
-        console.log(err.response.data.error);
-        toast.error(err.response.data.error);
-      });
-  };
   const onChange = (e) => {
-    console.log(e.target.name);
-    console.log(e.target.value);
+    //console.log(e.target.name);
+    //console.log(e.target.value);
     setValue({
       ...value,
       [e.target.name]: e.target.value,
     });
+    //console.log(value);
+  };
+
+  const onClick = (e) => {
+    e.preventDefault(); //
+    //console.log(value);
+
+    createQuestion(value)
+      .then((res) => {
+        //console.log(res.data);
+        toast.success(res.data.status);
+        loadData();
+      })
+      .catch((err) => {
+        //console.log(err.response.data.error);
+        toast.error(err.response.data.error);
+      });
   };
 
   const handleRemove = (id) => {
     if (window.confirm("Are You Sure Delete")) {
       removeQuestion(id)
         .then((res) => {
-          console.log(res.data.status);
+          //console.log(res.data.status);
           toast.success(res.data.status);
           loadData();
         })
         .catch((err) => {
-          console.log(err.response.data);
+          //console.log(err.response.data.error);
+          toast.error(err.response.data.error);
         });
     }
   };
-  const [selectedType, setSelectedType] = useState([]);
-  const handleChangeType = (value) => {
+  const [selectedType, setSelectedType] = useState();
+  const handleChangeType = (id) => {
+    setSelectedType(id);
     let values = {
-      type_id: value,
+      type_id: id,
     };
-    setSelectedType(value);
-    setValue({
-      type_id: values.type_id,
-    });
-    console.log(values);
+    setValue({ ...value, type_id: values.type_id });
+  };
+
+  //======pop up edit===============
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = (id, detail, name) => {
+    setIsModalOpen(true);
+    setEditDetail({ ...editDetail, id: id });
+    setDetailOld(detail);
+    setDetailTypeOld(name);
+    setEditTypeDetail({ ...editTypeDetail, id: id });
+    //console.log(id);
+  };
+
+  const [detailOld, setDetailOld] = useState();
+  const [detailTypeOld, setDetailTypeOld] = useState();
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  const [editDetail, setEditDetail] = useState({
+    id: "",
+    detail: "",
+  });
+  const handleChangeDetail = (e) => {
+    //console.log(e.target.name);
+    //console.log(e.target.value);
+    setEditDetail({ ...editDetail, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmitDetail = () => {
+    //console.log(editDetail);
+    changeQuestion(editDetail)
+      .then((res) => {
+        //console.log(res.data.status);
+        toast.success(res.data.status);
+        loadData();
+
+        setIsModalOpen(false);
+      })
+      .catch((err) => {
+        //console.log(err.response.data.error);
+        toast.error(err.response.data.error);
+      });
+  };
+  const [editTypeDetail, setEditTypeDetail] = useState({
+    id: "",
+    type_id: "",
+  });
+  const [selectedEditType, setSelectedEditType] = useState("ทั้งหมด");
+  const handleChangeEditType = (id) => {
+    let values = {
+      type_id: id,
+    };
+    setSelectedEditType(id);
+    setEditTypeDetail({ ...editTypeDetail, type_id: values.type_id });
+  };
+  const handleSubmitTypeDetail = () => {
+    //console.log(editTypeDetail);
+    changeType(editTypeDetail)
+      .then((res) => {
+        //console.log(res.data.status);
+        toast.success(res.data.status);
+        loadData();
+
+        setIsModalOpen(false);
+      })
+      .catch((err) => {
+        //console.log(err.response.data.error);
+        toast.error(err.response.data.error);
+      });
   };
 
   return (
@@ -175,7 +248,7 @@ const Question = () => {
                           width: "100%",
                         }}
                         size="large"
-                        onChange={(value) => handleChangeType(value)}
+                        onChange={(id) => handleChangeType(id)}
                         value={selectedType}
                         //onChange={handleChange}
                       >
@@ -218,37 +291,22 @@ const Question = () => {
                     <tbody>
                       {questions.map((item, index) => (
                         <tr key={index}>
-                          {/** 
-
-                          <th scope="row">{index + 1}</th>
-                          <td>{item.detail}</td>
-
-                          <td>
-                            {item.name}
-                            {item.type_id}
-                          </td>
-                          <td>
-                            <button className="btn btn-warning btn-sm me-3">
-                              แก้ไข
-                            </button>
-                            <button
-                              className="btn btn-danger btn-sm"
-                              onClick={() => handleRemove(item.id)}
-                            >
-                              ลบ
-                            </button>
-                          </td>
-*/}
-
                           {selected === "ทั้งหมด" ? (
                             <>
                               <th scope="row">{index + 1}</th>
                               <td>{item.detail}</td>
                               <td>{item.name}</td>
                               <td>
-                                <button className="btn btn-warning btn-sm me-3">
+                                <Button
+                                  type="primary"
+                                  style={{ marginRight: "1rem" }}
+                                  onClick={() =>
+                                    showModal(item.id, item.detail, item.name)
+                                  }
+                                >
                                   แก้ไข
-                                </button>
+                                </Button>
+
                                 <button
                                   className="btn btn-danger btn-sm"
                                   onClick={() => handleRemove(item.id)}
@@ -268,9 +326,16 @@ const Question = () => {
 
                               <td>{item.name}</td>
                               <td>
-                                <button className="btn btn-warning btn-sm me-3">
+                                <Button
+                                  type="primary"
+                                  style={{ marginRight: "1rem" }}
+                                  onClick={() =>
+                                    showModal(item.id, item.detail, item.name)
+                                  }
+                                >
                                   แก้ไข
-                                </button>
+                                </Button>
+
                                 <button
                                   className="btn btn-danger btn-sm"
                                   onClick={() => handleRemove(item.id)}
@@ -286,7 +351,65 @@ const Question = () => {
                       ))}
                     </tbody>
                   </table>
+                  <Modal
+                    title="แก้ไขคำถาม"
+                    open={isModalOpen}
+                    footer={null}
+                    onCancel={handleCancel}
+                  >
+                    <label>
+                      <b>คำถาม : {detailOld}</b>
+                    </label>
+                    <br />
+                    <br />
+                    <Input
+                      type="text"
+                      name="detail"
+                      placeholder={" แก้ไขคำถาม"}
+                      onChange={handleChangeDetail}
+                      prefix={
+                        <FontSizeOutlined className="site-form-item-icon" />
+                      }
+                    />
+                    <Button
+                      type="primary"
+                      style={{ marginTop: "0.5rem" }}
+                      onClick={handleSubmitDetail}
+                    >
+                      เปลี่ยน
+                    </Button>
+                    <br />
+                    <br />
+                    <hr />
 
+                    <label>
+                      <b>หมวดหมู่ : {detailTypeOld}</b>
+                    </label>
+                    <br />
+                    <br />
+                    <Select
+                      defaultValue="ทั้งหมด"
+                      style={{
+                        width: "100%",
+                      }}
+                      onChange={(value) => handleChangeEditType(value)}
+                      value={selectedEditType}
+                      //onChange={handleChange}
+                    >
+                      {typeData.map((item, index) => (
+                        <Option value={item.type_id} key={index}>
+                          {item.name}
+                        </Option>
+                      ))}
+                    </Select>
+                    <Button
+                      type="primary"
+                      style={{ marginTop: "0.5rem" }}
+                      onClick={handleSubmitTypeDetail}
+                    >
+                      เปลี่ยน
+                    </Button>
+                  </Modal>
                   {/**================================== */}
                 </div>
               </div>
